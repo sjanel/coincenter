2073fe894bb4f67163cf41b9385dde630118c30c
Stephane Janel <stephane.janel@amadeus.com>
2021-05-03 15:13:26 +0200

    Structured command line options

--- src/engine/include/coincenteroptions.hpp
+++ src/engine/include/coincenteroptions.hpp
@@ -4,13 +4,10 @@
 #include <string>
 #include <utility>
 
-#include "cct_smallvector.hpp"
 #include "commandlineoptionsparser.hpp"
 #include "currencycode.hpp"
-#include "exchangename.hpp"
-#include "market.hpp"
-#include "monetaryamount.hpp"
 #include "tradeoptionsapi.hpp"
+#include "wallet.hpp"
 
 namespace cct {
 
@@ -18,8 +15,6 @@ struct CoincenterCmdLineOptions {
   void setLogLevel() const;
 
   void setLogFile() const;
-
-  static void PrintUsage(const char* programName, const char* error = nullptr);
 
   static void PrintVersion(const char* programName);
 
@@ -49,29 +44,73 @@ struct CoincenterCmdLineOptions {
   std::string withdraw{};
 };
 
-inline CoincenterCmdLineOptions ParseCoincenterCmdLineOptions(const vector<std::string_view>& vargv) {
-  auto parser = CmdOpts<CoincenterCmdLineOptions>::Create(
-      {{"--help", &CoincenterCmdLineOptions::help},
-       {"--version", &CoincenterCmdLineOptions::version},
-       {"--loglevel", &CoincenterCmdLineOptions::logLevel},
-       {"--logfile", &CoincenterCmdLineOptions::logFile},
+template <class OptValueType>
+inline CommandLineOptionsParser<OptValueType> CreateCoincenterCommandLineOptionsParser() {
+  constexpr int64_t defaultTradeTimeout =
+      std::chrono::duration_cast<std::chrono::seconds>(api::TradeOptions::kDefaultTradeDuration).count();
+  constexpr int64_t emergencyBufferTime =
+      std::chrono::duration_cast<std::chrono::milliseconds>(api::TradeOptions::kDefaultEmergencyTime).count();
+  constexpr int64_t minUpdatePriceTime =
+      std::chrono::duration_cast<std::chrono::milliseconds>(api::TradeOptions::kDefaultMinTimeBetweenPriceUpdates)
+          .count();
+  const bool isSimulationModeByDefault = api::TradeOptions().simulation();
 
-       {"--orderbook", &CoincenterCmdLineOptions::orderbook},
-       {"--orderbook-depth", &CoincenterCmdLineOptions::orderbook_depth},
-       {"--orderbook-cur", &CoincenterCmdLineOptions::orderbook_cur},
+  // clang-format off
+  return CommandLineOptionsParser<OptValueType>(
+      {{{{"General", 1}, "--help", 'h', "", "Display this information"}, &OptValueType::help},
+       {{{"General", 1}, "--version", "", "Display program version"}, &OptValueType::version},
+       {{{"General", 1}, "--loglevel", "<levelname>", "Sets the log level during all execution. "
+                                                      "Possible values are: trace|debug|info|warning|error|critical|off"}, 
+                                                      &OptValueType::logLevel},
+       {{{"General", 1}, "--logfile", "", "Log to rotating files instead of stdout / stderr"}, &OptValueType::logFile},
 
-       {"--balance", &CoincenterCmdLineOptions::balance},
-       {"--balance-cur", &CoincenterCmdLineOptions::balance_cur},
+       {{{"Public queries", 2}, "--orderbook", 'o', "<cur1-cur2,exch1,...>", "Print order book of currency pair on a list of exchanges"}, 
+                                                                             &OptValueType::orderbook},
+       {{{"Public queries", 2}, "--orderbook-depth", "", "Override default depth of order book"}, &OptValueType::orderbook_depth},
+       {{{"Public queries", 2}, "--orderbook-cur", "<cur code>", "If conversion of cur2 into cur is possible on exch1, "
+                                                                 "prints additional column converted to given asset"}, 
+                                                                 &OptValueType::orderbook_cur},
 
-       {"--trade", &CoincenterCmdLineOptions::trade},
-       {"--trade-strategy", &CoincenterCmdLineOptions::trade_strategy},
-       {"--trade-timeout", &CoincenterCmdLineOptions::trade_timeout_s},
-       {"--trade-emergency", &CoincenterCmdLineOptions::trade_emergency_ms},
-       {"--trade-updateprice", &CoincenterCmdLineOptions::trade_updateprice_ms},
-       {"--trade-sim", &CoincenterCmdLineOptions::trade_sim},
+       {{{"Private queries", 3}, "--balance", 'b', "<exch1,...|all>", "Prints available balance on given exchanges. "
+                                                                      "If 'all' is specified, sums all available private exchange accounts"}, 
+                                                                      &OptValueType::balance},
+       {{{"Private queries", 3}, "--balance-cur", "<cur code>", "Print additional information with each asset "
+                                                                "converted to given currency, plus a total summary"}, 
+                                                                &OptValueType::balance_cur},
 
-       {"--withdraw", &CoincenterCmdLineOptions::withdraw}});
-  return parser.parse(vargv);
+       {{{"Trade", 4}, "--trade", 't', "<amt cur1-cur2,exchange>", "Single trade from given start amount on an exchange. "
+                                                                   "Order will be placed at limit price by default"}, &OptValueType::trade},
+       {{{"Trade", 4}, "--trade-strategy", "<maker|taker|adapt>", "Customize the strategy of the trade\n"
+                                                                  " - 'maker': order placed at limit price (default), continuously "
+                                                                  "adjusted to limit price\n"
+                                                                  " - 'taker': order placed at market price should be matched directly\n"
+                                                                  " - 'adapt': same as maker, except that order will be updated"
+                                                                  " at market price before the timeout to make it eventually completely matched"}, 
+                                                                  &OptValueType::trade_strategy},
+       {{{"Trade", 4}, "--trade-timeout", "<s>", std::string("Adjust trade timeout (default: ")
+                                                .append(std::to_string(defaultTradeTimeout))
+                                                .append("). Remaining orders will be cancelled after the timeout.")}, 
+                                                &OptValueType::trade_timeout_s},
+       {{{"Trade", 4}, "--trade-emergency", "<ms>", std::string("Adjust emergency buffer for the 'adapt' strategy (default: ")
+                                                    .append(std::to_string(emergencyBufferTime))
+                                                    .append("). Remaining order will be switched from limit to market price "
+                                                    "after 'timeout - emergency' time to force completion of the trade")}, 
+                                                    &OptValueType::trade_emergency_ms},
+       {{{"Trade", 4}, "--trade-updateprice", "<ms>", std::string("Set the min time allowed between two limit price updates (default: ")
+                                                     .append(std::to_string(minUpdatePriceTime))
+                                                     .append("). Avoids cancelling / placing new orders too often with high volumes "
+                                                     "which can be counter productive sometimes.")}, &OptValueType::trade_updateprice_ms},
+       {{{"Trade", 4}, "--trade-sim", "", std::string("Activates simulation mode only (default: ")
+                                         .append(isSimulationModeByDefault ? "true" : "false")
+                                         .append("). For some exchanges, API can even be queried in this "
+                                         "mode to ensure deeper and more realistic trading inputs")}, &OptValueType::trade_sim},
+
+       {{{"Withdraw crypto", 5}, "--withdraw", 'w', "<amt cur,from-to>", std::string("Withdraw amount from exchange 'from' to exchange 'to'."
+                                                                         " Amount is gross, including fees. Address and tag will be "
+                                                                         "retrieved automatically from '")
+                                                                        .append(Wallet::kDepositAddressesFilename)
+                                                                        .append(".json' file. Make sure that values are up to date (and "
+                                                                        "correct of course!)")}, &OptValueType::withdraw}});
+  // clang-format on
 }
-
 }  // namespace cct
--- src/tools/test/commandlineoptionsparser_test.cpp
+++ src/tools/test/commandlineoptionsparser_test.cpp
@@ -3,33 +3,58 @@
 
 #include <gtest/gtest.h>
 
+#include "cct_vector.hpp"
+
 namespace cct {
-TEST(CommandLineOptionsParser, Basic) {
+
+class CommandLineOptionsParserTest : public ::testing::Test {
+ public:
   struct Opts {
     std::string stringOpt{};
     int intOpt{};
+    int int2Opt{};
     bool boolOpt{};
   };
 
-  CmdOpts parser =
-      CmdOpts<Opts>::Create({{"--opt1", &Opts::stringOpt}, {"--opt2", &Opts::intOpt}, {"--opt3", &Opts::boolOpt}});
+  using ParserType = CommandLineOptionsParser<Opts>;
 
-  Opts options = parser.parse({"coincenter", "--opt1", "toto", "--opt3"});
+  CommandLineOptionsParserTest()
+      : _parser({{{{"General", 1}, "--opt1", 'o', "<myValue>", "Opt1 descr"}, &Opts::stringOpt},
+                 {{{"General", 1}, "--opt2", "", "Opt2 descr"}, &Opts::intOpt},
+                 {{{"Other", 2}, "--opt3", "", "Opt3 descr"}, &Opts::int2Opt},
+                 {{{"General", 1}, "--help", 'h', "", "Help descr"}, &Opts::boolOpt}}) {}
+
+ protected:
+  virtual void SetUp() {}
+  virtual void TearDown() {}
+
+  ParserType _parser;
+};
+
+TEST_F(CommandLineOptionsParserTest, Basic) {
+  cct::vector<const char *> opts1 = {"coincenter", "--opt1", "toto", "--help"};
+  Opts options = _parser.parse(opts1);
   EXPECT_EQ(options.stringOpt, "toto");
   EXPECT_TRUE(options.boolOpt);
 
-  EXPECT_THROW(parser.parse({"coincenter", "--opt1", "toto", "--opt3", "--opt2"}), std::invalid_argument);
-  EXPECT_THROW(parser.parse({"coincenter", "--opt1", "toto", "--opts3", "--opt2", "3"}), std::invalid_argument);
+  cct::vector<const char *> opts2 = {"coincenter", "--opt1", "toto", "--opt3", "--opt2"};
+  EXPECT_THROW(_parser.parse(opts2), std::invalid_argument);
+  cct::vector<const char *> opts3 = {"coincenter", "--opt1", "toto", "--opts3", "--opt2", "3"};
+  EXPECT_THROW(_parser.parse(opts3), std::invalid_argument);
 }
 
-TEST(CommandLineOptionsParser, String) {
-  struct Opts {
-    std::string stringOpt{};
-  };
-
-  CmdOpts parser = CmdOpts<Opts>::Create({{"--opt1", &Opts::stringOpt}});
-
-  Opts options = parser.parse({"coincenter", "--opt1", "2000 EUR, kraken"});
+TEST_F(CommandLineOptionsParserTest, String) {
+  cct::vector<const char *> opts = {"coincenter", "--opt1", "2000 EUR, kraken"};
+  Opts options = _parser.parse(opts);
   EXPECT_EQ(options.stringOpt, "2000 EUR, kraken");
 }
+
+TEST_F(CommandLineOptionsParserTest, AlternativeOptionName) {
+  cct::vector<const char *> opts = {"coincenter", "-h"};
+  Opts options = _parser.parse(opts);
+  EXPECT_TRUE(options.boolOpt);
+  cct::vector<const char *> opts2 = {"coincenter", "-j"};
+  EXPECT_THROW(_parser.parse(opts2), std::invalid_argument);
+}
+
 }  // namespace cct
