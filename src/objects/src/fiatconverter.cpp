4273d7dbc1a4802ba0e18aced559f776f205e0d7
Stephane Janel <stephane.janel@amadeus.com>
2021-05-29 15:07:02 +0200

    Adds possibility to use custom free currency converter key to avoid reaching limits of hardcoded one

--- src/objects/src/fiatconverter.cpp
+++ src/objects/src/fiatconverter.cpp
@@ -9,13 +9,29 @@ namespace cct {
 namespace {
 constexpr char kRatesFileName[] = ".ratescache.json";
 constexpr char kCurrencyConverterBaseUrl[] = "https://free.currconv.com/api/v7";
-constexpr char kAPIKey[] = "b25453de7984135a084b";
-// example http://free.currconv.com/api/v7/currencies?apiKey=b25453de7984135a084b
+constexpr char kFiatConverterJsonKeyFile[] = "thirdparty_secret.json";
+
+std::string LoadCurrencyConverterAPIKey() {
+  constexpr char kDefaultCommunityKey[] = "b25453de7984135a084b";
+  // example http://free.currconv.com/api/v7/currencies?apiKey=b25453de7984135a084b
+
+  json data = OpenJsonFile(kFiatConverterJsonKeyFile, FileNotFoundMode::kNoThrow, FileType::kConfig);
+  if (data.empty() || data["freecurrencyconverter"].get<std::string_view>() == kDefaultCommunityKey) {
+    log::warn("Unable to find custom Free Currency Converter key in {}", kFiatConverterJsonKeyFile);
+    log::warn("If you want to use extensively coincenter, please create your own key by going to");
+    log::warn("https://free.currencyconverterapi.com/free-api-key and place it in");
+    log::warn("'config/thirdparty_secret.json' like this:");
+    log::warn("  {\"freecurrencyconverter\": \"<YourKey>\"}");
+    log::warn("Using default key provided as a demo to the community");
+    return kDefaultCommunityKey;
+  }
+  return std::string(data["freecurrencyconverter"].get<std::string_view>());
+}
 
 }  // namespace
 
 FiatConverter::FiatConverter(Clock::duration ratesUpdateFrequency, bool loadFromFileCacheAtInit)
-    : _ratesUpdateFrequency(ratesUpdateFrequency) {
+    : _ratesUpdateFrequency(ratesUpdateFrequency), _apiKey(LoadCurrencyConverterAPIKey()) {
   if (loadFromFileCacheAtInit) {
     json data = OpenJsonFile(kRatesFileName, FileNotFoundMode::kNoThrow, FileType::kData);
     _pricesMap.reserve(data.size());
@@ -23,9 +39,10 @@ FiatConverter::FiatConverter(Clock::duration ratesUpdateFrequency, bool loadFrom
       Market m(marketStr, '-');
       double rate = rateAndTimeData["rate"];
       int64_t timeepoch = rateAndTimeData["timeepoch"];
-      log::debug("Stored rate {} for market {} from cache file", rate, marketStr);
+      log::trace("Stored rate {} for market {} from {}", rate, marketStr, kRatesFileName);
       _pricesMap.insert_or_assign(m, PriceTimedValue{rate, TimePoint(std::chrono::seconds(timeepoch))});
     }
+    log::debug("Loaded {} fiat currency rates from {}", _pricesMap.size(), kRatesFileName);
   }
 }
 
@@ -47,7 +64,7 @@ std::optional<double> FiatConverter::queryCurrencyRate(Market m) {
   CurlOptions opts(CurlOptions::RequestType::kGet);
   std::string qStr(m.assetsPairStr('_'));
   opts.postdata.append("q", qStr);
-  opts.postdata.append("apiKey", kAPIKey);
+  opts.postdata.append("apiKey", _apiKey);
 
   url.append(opts.postdata.c_str());
   opts.postdata.clear();
@@ -83,9 +100,12 @@ double FiatConverter::convert(double amount, CurrencyCode from, CurrencyCode to)
   double rate;
   std::lock_guard<std::mutex> guard(_pricesMutex);
   auto it = _pricesMap.find(m);
-  if (it != _pricesMap.end() && Clock::now() < it->second.lastUpdatedTime + _ratesUpdateFrequency) {
+  if (it != _pricesMap.end() && Clock::now() - it->second.lastUpdatedTime < _ratesUpdateFrequency) {
     rate = it->second.rate;
   } else {
+    if (_ratesUpdateFrequency == Clock::duration::max()) {
+      throw exception("Unable to query fiat currency rates and no rate found in cache");
+    }
     std::optional<double> queriedRate = queryCurrencyRate(m);
     if (queriedRate) {
       rate = *queriedRate;
