e6b9e462b5cafa0e1e7b5529d347b01e175c8197
Stephane Janel <stephane.janel@amadeus.com>
2021-09-01 14:19:17 +0200

    Since nlohmann::json 3.10, ArrayType cannot be defined with a custom vector not accepting incomplete types (amc::vector does not work with incomplete types)

--- src/tools/include/cct_json.hpp
+++ src/tools/include/cct_json.hpp
@@ -4,12 +4,13 @@
 #include <map>
 #include <nlohmann/json.hpp>
 #include <string>
+#include <vector>
 
 #include "cct_vector.hpp"
 
 namespace cct {
 
-using json = nlohmann::basic_json<std::map, cct::vector, std::string, bool, std::int64_t, std::uint64_t, double,
+using json = nlohmann::basic_json<std::map, std::vector, std::string, bool, std::int64_t, std::uint64_t, double,
                                   amc::allocator, nlohmann::adl_serializer, cct::vector<std::uint8_t>>;
 
 }  // namespace cct
