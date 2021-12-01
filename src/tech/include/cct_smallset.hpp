cd0e84cb6565daed56c45b92fec3765d47151c16
Stephane Janel <stephane.janel@amadeus.com>
2021-12-01 09:11:58 +0100

    Update AMC version

--- src/tech/include/cct_smallset.hpp
+++ src/tech/include/cct_smallset.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+#define AMC_NONSTD_FEATURES
 #include <amc/smallset.hpp>
+#undef AMC_NONSTD_FEATURES
 
 namespace cct {
 using amc::SmallSet;
--- src/tech/include/cct_vector.hpp
+++ src/tech/include/cct_vector.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+#define AMC_NONSTD_FEATURES
 #include <amc/vector.hpp>
+#undef AMC_NONSTD_FEATURES
 
 namespace cct {
 using amc::vector;
