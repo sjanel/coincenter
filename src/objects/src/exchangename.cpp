659598816b0de1afff0526dda84baae47b8d8fc5
Stephane Janel <stephane.janel@amadeus.com>
2021-12-26 19:55:41 +0100

    Make sure exchange name is lower case

--- src/objects/src/exchangename.cpp
+++ src/objects/src/exchangename.cpp
@@ -1,6 +1,7 @@
 #include "exchangename.hpp"
 
 #include "cct_exception.hpp"
+#include "toupperlower.hpp"
 
 namespace cct {
 PrivateExchangeName::PrivateExchangeName(std::string_view globalExchangeName)
@@ -8,10 +9,12 @@ PrivateExchangeName::PrivateExchangeName(std::string_view globalExchangeName)
   if (_dashPos == std::string_view::npos) {
     _dashPos = _nameWithKey.size();
   }
+  std::transform(_nameWithKey.begin(), _nameWithKey.begin() + _dashPos, _nameWithKey.begin(),
+                 [](char c) { return tolower(c); });
 }
 
 PrivateExchangeName::PrivateExchangeName(std::string_view exchangeName, std::string_view keyName)
-    : _nameWithKey(exchangeName), _dashPos(_nameWithKey.size()) {
+    : _nameWithKey(tolower(exchangeName)), _dashPos(_nameWithKey.size()) {
   if (_nameWithKey.find('_') != string::npos) {
     throw exception("Invalid exchange name " + _nameWithKey);
   }
