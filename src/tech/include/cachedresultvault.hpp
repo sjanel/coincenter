17f605a66c7aa632d419b8f9c3c61bcbd12e55c0
Stephane Janel <stephane.janel@gmail.com>
2022-02-12 11:57:22 +0100

    Make CachedResult test more deterministic by using steady_clock

--- src/tech/include/cachedresultvault.hpp
+++ src/tech/include/cachedresultvault.hpp
@@ -4,38 +4,60 @@
 #include "timedef.hpp"
 
 namespace cct {
+template <class DurationT>
 class CachedResultBase {
  protected:
-  friend class CachedResultVault;
+  template <class>
+  friend class CachedResultVaultT;
 
   enum class State { kStandardRefresh, kForceUniqueRefresh, kForceCache };
 
-  explicit CachedResultBase(Duration refreshPeriod) : _refreshPeriod(refreshPeriod) {}
+  explicit CachedResultBase(DurationT refreshPeriod) : _refreshPeriod(refreshPeriod) {}
 
   void freeze() noexcept { _state = State::kForceUniqueRefresh; }
 
   void unfreeze() noexcept { _state = State::kStandardRefresh; }
 
-  Duration _refreshPeriod;
+  DurationT _refreshPeriod;
   State _state = State::kStandardRefresh;
 };
 
 /// Represents an Observer of CachedResults.
 /// It can be used to launch queries on all objects listening to this observer.
-class CachedResultVault {
+template <class DurationT>
+class CachedResultVaultT {
  public:
-  CachedResultVault() noexcept = default;
+  CachedResultVaultT() noexcept = default;
 
-  void registerCachedResult(CachedResultBase &cacheResult);
+  void registerCachedResult(CachedResultBase<DurationT> &cacheResult) {
+    _cachedResults.push_back(std::addressof(cacheResult));
+  }
 
-  void freezeAll();
+  void freezeAll() {
+    if (!_allFrozen) {
+      for (CachedResultBase<DurationT> *p : _cachedResults) {
+        p->freeze();
+      }
+      _allFrozen = true;
+    }
+  }
 
-  void unfreezeAll() noexcept;
+  void unfreezeAll() noexcept {
+    if (_allFrozen) {
+      for (CachedResultBase<DurationT> *p : _cachedResults) {
+        p->unfreeze();
+      }
+      _allFrozen = false;
+    }
+  }
 
  private:
-  using CachedResultPtrs = vector<CachedResultBase *>;
+  using CachedResultPtrs = vector<CachedResultBase<DurationT> *>;
 
   CachedResultPtrs _cachedResults;
   bool _allFrozen = false;
 };
+
+using CachedResultVault = CachedResultVaultT<Duration>;
+
 }  // namespace cct
--- src/tech/include/timedef.hpp
+++ src/tech/include/timedef.hpp
@@ -5,6 +5,7 @@
 namespace cct {
 /// Alias some types to make it easier to use
 /// The main clock is system_clock as it is the only one guaranteed to provide conversions to Unix epoch time.
+/// It is not monotonic - thus for unit tests we will prefer usage of steady_clock
 using Clock = std::chrono::system_clock;
 using TimePoint = Clock::time_point;
 using Duration = Clock::duration;
