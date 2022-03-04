9a36a74a3a70b0000dbcb5fc4974236bcc4800d9
Stephane Janel <stephane.janel@gmail.com>
2022-03-04 08:49:31 +0100

    Fix conversion method bug introduced in 3.14.0

--- src/api/common/test/exchangepublicapi_test.cpp
+++ src/api/common/test/exchangepublicapi_test.cpp
@@ -12,24 +12,21 @@
 namespace cct::api {
 class ExchangePublicTest : public ::testing::Test {
  protected:
-  ExchangePublicTest()
-      : cryptowatchAPI(coincenterInfo),
-        fiatConverter(coincenterInfo, Duration::max()),  // max to avoid real Fiat converter queries
-        exchangePublic(kSupportedExchanges[0], fiatConverter, cryptowatchAPI, coincenterInfo) {}
-
   virtual void SetUp() {}
   virtual void TearDown() {}
 
   CoincenterInfo coincenterInfo;
-  CryptowatchAPI cryptowatchAPI;
-  FiatConverter fiatConverter;
-  MockExchangePublic exchangePublic;
+  CryptowatchAPI cryptowatchAPI{coincenterInfo};
+  FiatConverter fiatConverter{coincenterInfo, Duration::max()};  // max to avoid real Fiat converter queries
+  MockExchangePublic exchangePublic{kSupportedExchanges[0], fiatConverter, cryptowatchAPI, coincenterInfo};
 };
 
 namespace {
 using MarketsPath = ExchangePublic::MarketsPath;
 using CurrenciesPath = ExchangePublic::CurrenciesPath;
 using MarketSet = ExchangePublic::MarketSet;
+using Fiats = ExchangePublic::Fiats;
+using MarketOrderBookMap = ExchangePublic::MarketOrderBookMap;
 }  // namespace
 
 TEST_F(ExchangePublicTest, FindFastestConversionPath) {
@@ -50,12 +47,82 @@ TEST_F(ExchangePublicTest, FindFastestConversionPath) {
 
 TEST_F(ExchangePublicTest, RetrieveMarket) {
   MarketSet markets{{"BTC", "KRW"}, {"XLM", "KRW"}, {"USD", "EOS"}};
-  EXPECT_CALL(exchangePublic, queryTradableMarkets()).Times(2).WillRepeatedly(::testing::Return(markets));
+  EXPECT_CALL(exchangePublic, queryTradableMarkets()).WillOnce(::testing::Return(markets));
 
   EXPECT_EQ(exchangePublic.retrieveMarket("BTC", "KRW"), Market("BTC", "KRW"));
-  MarketSet tradableMarkets = exchangePublic.queryTradableMarkets();
-  EXPECT_EQ(exchangePublic.retrieveMarket("KRW", "BTC", tradableMarkets), Market("BTC", "KRW"));
-  EXPECT_THROW(exchangePublic.retrieveMarket("EUR", "EOS", tradableMarkets), exception);
+  EXPECT_EQ(exchangePublic.retrieveMarket("KRW", "BTC", markets), Market("BTC", "KRW"));
+  EXPECT_THROW(exchangePublic.retrieveMarket("EUR", "EOS", markets), exception);
+}
+
+class ExchangePublicConvertTest : public ExchangePublicTest {
+ protected:
+  Fiats fiats{"EUR", "USD", "KRW"};
+
+  VolAndPriNbDecimals volAndPriDec1{2, 6};
+  int depth = 10;
+
+  MonetaryAmount askPrice1{"0.000017 BTC"};
+  MonetaryAmount bidPrice1{"0.000016 BTC"};
+  MarketOrderBook marketOrderBook1{
+      askPrice1, MonetaryAmount(40000, "XLM"), bidPrice1, MonetaryAmount(25000, "XLM"), volAndPriDec1, depth};
+
+  VolAndPriNbDecimals volAndPriDec2{2, 4};
+
+  MonetaryAmount askPrice2{"0.0063 BTC"};
+  MonetaryAmount bidPrice2{"0.0062 BTC"};
+  MarketOrderBook marketOrderBook2{
+      askPrice2, MonetaryAmount(680, "XRP"), bidPrice2, MonetaryAmount(1479, "XRP"), volAndPriDec2, depth};
+
+  VolAndPriNbDecimals volAndPriDec3{2, 2};
+
+  MonetaryAmount askPrice3{"37.5 EUR"};
+  MonetaryAmount bidPrice3{"37.49 EUR"};
+  MarketOrderBook marketOrderBook3{
+      askPrice3, MonetaryAmount("12.04 SOL"), bidPrice3, MonetaryAmount("0.45 SOL"), volAndPriDec3, depth};
+
+  MarketOrderBookMap marketOrderBookMap{{Market("XLM", "BTC"), marketOrderBook1},
+                                        {Market("XRP", "BTC"), marketOrderBook2},
+                                        {Market("SOL", "EUR"), marketOrderBook3}};
+
+  bool canUseCryptowatchAPI = false;
+  PriceOptions priceOptions;
+};
+
+TEST_F(ExchangePublicConvertTest, ConvertImpossible) {
+  MonetaryAmount from{50000, "XLM"};
+  CurrencyCode toCurrency{"BTC"};
+  MarketsPath conversionPath{};
+
+  std::optional<MonetaryAmount> ret = exchangePublic.convert(from, toCurrency, conversionPath, fiats,
+                                                             marketOrderBookMap, canUseCryptowatchAPI, priceOptions);
+  ASSERT_FALSE(ret.has_value());
+}
+
+TEST_F(ExchangePublicConvertTest, ConvertSimple) {
+  MonetaryAmount from{50000, "XLM"};
+  CurrencyCode toCurrency{"BTC"};
+  MarketsPath conversionPath{{"XLM", "BTC"}};
+
+  std::optional<MonetaryAmount> ret = exchangePublic.convert(from, toCurrency, conversionPath, fiats,
+                                                             marketOrderBookMap, canUseCryptowatchAPI, priceOptions);
+  ASSERT_TRUE(ret.has_value());
+  MonetaryAmount res = exchangePublic.exchangeInfo().applyFee(*marketOrderBook1.convert(from, priceOptions),
+                                                              ExchangeInfo::FeeType::kMaker);
+  EXPECT_EQ(*ret, res);
+}
+
+TEST_F(ExchangePublicConvertTest, ConvertDouble) {
+  MonetaryAmount from{50000, "XLM"};
+  CurrencyCode toCurrency{"XRP"};
+  MarketsPath conversionPath{{"XLM", "BTC"}, {"XRP", "BTC"}};
+  std::optional<MonetaryAmount> ret = exchangePublic.convert(from, toCurrency, conversionPath, fiats,
+                                                             marketOrderBookMap, canUseCryptowatchAPI, priceOptions);
+  ASSERT_TRUE(ret.has_value());
+  MonetaryAmount res = exchangePublic.exchangeInfo().applyFee(*marketOrderBook1.convert(from, priceOptions),
+                                                              ExchangeInfo::FeeType::kMaker);
+  res = exchangePublic.exchangeInfo().applyFee(*marketOrderBook2.convert(res, priceOptions),
+                                               ExchangeInfo::FeeType::kMaker);
+  EXPECT_EQ(*ret, res);
 }
 
 }  // namespace cct::api
