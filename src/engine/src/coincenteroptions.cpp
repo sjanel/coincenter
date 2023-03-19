daae09f16f0ee558e9cb8269d9f05b6d2773dabf
Stephane Janel <stephane.janel@amadeus.com>
2023-03-19 15:44:27 +0100

    Allow CCT_DATA_DIR environment variable to be read at runtime

--- src/engine/src/coincenteroptions.cpp
+++ src/engine/src/coincenteroptions.cpp
@@ -1,5 +1,6 @@
 #include "coincenteroptions.hpp"
 
+#include <cstdlib>
 #include <iostream>
 
 #include "cct_const.hpp"
@@ -8,7 +9,15 @@
 
 namespace cct {
 
-void CoincenterCmdLineOptions::PrintVersion(std::string_view programName) {
+std::string_view SelectDefaultDataDir() noexcept {
+  const char *pDataDirEnvValue = std::getenv("CCT_DATA_DIR");
+  if (pDataDirEnvValue != nullptr) {
+    return pDataDirEnvValue;
+  }
+  return kDefaultDataDir;
+}
+
+void CoincenterCmdLineOptions::PrintVersion(std::string_view programName) noexcept {
   std::cout << programName << " version " << CCT_VERSION << std::endl;
   std::cout << "compiled with " << CCT_COMPILER_VERSION << " on " << __DATE__ << " at " << __TIME__ << std::endl;
   std::cout << "              " << GetCurlVersionInfo() << std::endl;
