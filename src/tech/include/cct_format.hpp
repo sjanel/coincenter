f0b6d74ccfdd441e9a0b71ec835f155eaa2d9b70
Stephane Janel <stephane.janel@amadeus.com>
2024-03-25 19:28:41 +0100

    Add cct::format

--- src/tech/include/cct_format.hpp
+++ src/tech/include/cct_format.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #ifdef CCT_DISABLE_SPDLOG
+#include <string>
 #include <string_view>
 #else
 #include <spdlog/spdlog.h>
@@ -17,9 +18,16 @@ template <typename OutputIt, typename... T>
 auto format_to(OutputIt out, format_string<T...>, T&&...) -> OutputIt {
   return out;
 }
+
+template <typename... T>
+auto format(format_string<T...> fmt, [[maybe_unused]] T&&... args) -> std::string {
+  return std::string{fmt};
+}
+
 #else
 template <typename... Args>
 using format_string = spdlog::format_string_t<Args...>;
+using fmt::format;
 using fmt::format_to;
 #endif
 
