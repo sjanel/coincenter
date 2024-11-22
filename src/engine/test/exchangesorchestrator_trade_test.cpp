f1cddb195c0ab030416c97b54bf3fc488ffe34f7
Stephane Janel <stephane.janel@amadeus.com>
2024-11-22 23:18:52 +0100

    Make amount optional (defaults to 1) for conversion rate command line option

--- src/engine/test/exchangesorchestrator_trade_test.cpp
+++ src/engine/test/exchangesorchestrator_trade_test.cpp
@@ -577,7 +577,8 @@ TEST_F(ExchangeOrchestratorTradeTest, AllExchangesBuyAllOneMarketUnavailable) {
 
 TEST_F(ExchangeOrchestratorTradeTest, SingleExchangeSmartBuy) {
   // Fee is automatically applied on buy
-  MonetaryAmount endAmount = MonetaryAmount(1000, "XRP") * exchangePublic1.exchangeConfig().tradeFees.taker;
+  MonetaryAmount endAmount = exchangePublic1.exchangeConfig().tradeFees.applyFee(
+      MonetaryAmount(1000, "XRP"), schema::ExchangeTradeFeesConfig::FeeType::Taker);
   CurrencyCode toCurrency = endAmount.currencyCode();
   TradeSide side = TradeSide::kBuy;
 
@@ -596,8 +597,10 @@ TEST_F(ExchangeOrchestratorTradeTest, SingleExchangeSmartBuy) {
 
 TEST_F(ExchangeOrchestratorTradeTest, SingleExchangeSmartBuyTwoSteps) {
   // Fee is automatically applied on buy
-  MonetaryAmount endAmount = MonetaryAmount(1000, "XRP") * exchangePublic1.exchangeConfig().tradeFees.taker *
-                             exchangePublic1.exchangeConfig().tradeFees.taker;
+  auto feeType = schema::ExchangeTradeFeesConfig::FeeType::Taker;
+  MonetaryAmount endAmount = exchangePublic1.exchangeConfig().tradeFees.applyFee(MonetaryAmount(1000, "XRP"), feeType);
+  // Two fees applications
+  endAmount = exchangePublic1.exchangeConfig().tradeFees.applyFee(endAmount, feeType);
   CurrencyCode toCurrency = endAmount.currencyCode();
   TradeSide side = TradeSide::kBuy;
 
@@ -615,7 +618,8 @@ TEST_F(ExchangeOrchestratorTradeTest, SingleExchangeSmartBuyTwoSteps) {
 }
 
 TEST_F(ExchangeOrchestratorTradeTest, TwoExchangesSmartBuy) {
-  MonetaryAmount endAmount = MonetaryAmount(10000, "XLM") * exchangePublic1.exchangeConfig().tradeFees.taker;
+  MonetaryAmount endAmount = exchangePublic1.exchangeConfig().tradeFees.applyFee(
+      MonetaryAmount(10000, "XLM"), schema::ExchangeTradeFeesConfig::FeeType::Taker);
   CurrencyCode toCurrency = endAmount.currencyCode();
   TradeSide side = TradeSide::kBuy;
 
@@ -643,7 +647,8 @@ TEST_F(ExchangeOrchestratorTradeTest, TwoExchangesSmartBuy) {
 }
 
 TEST_F(ExchangeOrchestratorTradeTest, TwoExchangesSmartBuyNoMarketOnOneExchange) {
-  MonetaryAmount endAmount = MonetaryAmount(10000, "XLM") * exchangePublic1.exchangeConfig().tradeFees.taker;
+  MonetaryAmount endAmount = exchangePublic1.exchangeConfig().tradeFees.applyFee(
+      MonetaryAmount(10000, "XLM"), schema::ExchangeTradeFeesConfig::FeeType::Taker);
   CurrencyCode toCurrency = endAmount.currencyCode();
   TradeSide side = TradeSide::kBuy;
 
@@ -666,7 +671,8 @@ TEST_F(ExchangeOrchestratorTradeTest, TwoExchangesSmartBuyNoMarketOnOneExchange)
 }
 
 TEST_F(ExchangeOrchestratorTradeTest, ThreeExchangesSmartBuy) {
-  MonetaryAmount endAmount = MonetaryAmount(10000, "XLM") * exchangePublic1.exchangeConfig().tradeFees.taker;
+  MonetaryAmount endAmount = exchangePublic1.exchangeConfig().tradeFees.applyFee(
+      MonetaryAmount(10000, "XLM"), schema::ExchangeTradeFeesConfig::FeeType::Taker);
   CurrencyCode toCurrency = endAmount.currencyCode();
   TradeSide side = TradeSide::kBuy;
 
@@ -704,7 +710,8 @@ TEST_F(ExchangeOrchestratorTradeTest, ThreeExchangesSmartBuy) {
 
 TEST_F(ExchangeOrchestratorTradeTest, SmartBuyAllExchanges) {
   CurrencyCode toCurrency("XLM");
-  MonetaryAmount endAmount = MonetaryAmount(18800, toCurrency) * exchangePublic1.exchangeConfig().tradeFees.taker;
+  MonetaryAmount endAmount = exchangePublic1.exchangeConfig().tradeFees.applyFee(
+      MonetaryAmount(18800, toCurrency), schema::ExchangeTradeFeesConfig::FeeType::Taker);
   TradeSide side = TradeSide::kBuy;
 
   MonetaryAmount from1 = MonetaryAmount(5000, "USDT");
